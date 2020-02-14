eca0e0fca3f8f5bc707815525b1808a0045d052c
Link <link@blockabc.com>
2020-02-14 18:01:58 +0800

    feat: implement main functions

---
+++ src/index.ts
@@ -0,0 +1,188 @@
+import assert from 'assert'
+import { createWriteStream, createReadStream, promises as fs, constants as fsConst } from 'fs'
+import * as path from 'path'
+import { promisify } from 'util'
+
+import archiver from 'archiver'
+import chalk from 'chalk'
+import fse from 'fs-extra'
+import { default as globModule } from 'glob'
+import loglevel from 'loglevel'
+
+const logger = loglevel.getLogger('Main')
+logger.setLevel(loglevel.levels.INFO)
+
+const glob = promisify(globModule)
+
+export interface IMapPair {
+  from: string,
+  to: string,
+}
+
+export type IMap = IMapPair[]
+
+class Packager {
+
+  protected _pid: string
+  protected _map: IMap
+  protected _input = '.'
+  protected _output = '.'
+
+  get pid (): string {
+    return this._pid
+  }
+
+  get map (): IMap {
+    return this._map
+  }
+
+  get input (): string {
+    return this._input
+  }
+
+  get output (): string {
+    return this._output
+  }
+
+  public async create (
+    { pid, input, output, host = '', map = [], verbose = false }:
+    { pid: string, input: string, output: string, host?: string, map?: IMap, verbose?: boolean }
+  ) {
+    if (verbose) {
+      logger.setLevel(loglevel.levels.TRACE)
+    }
+
+    logger.debug(chalk.blue(`CWD: ${process.cwd()}`))
+
+    assert(pid && Number.parseInt(pid) > 0, 'Pid must be an integer bigger than 0.')
+    assert(input, 'Input directory must be a path.')
+    assert(host || map, 'Host and map can not be both empty.')
+
+    this._pid = pid
+
+    if (host) {
+      this._map = [{
+        from: '.',
+        to: host
+      }]
+      logger.debug(chalk.blue(`Convert host to map: ${JSON.stringify(this._map)}`))
+    }
+    else if (map) {
+      this._map = map
+    }
+
+    logger.info(chalk.green(`Validating input path and output path...`))
+
+    // Ensure input path is ok
+    this._input = path.resolve(input)
+    await fs.access(this._input, fsConst.X_OK | fsConst.R_OK)
+
+
+    logger.debug(chalk.blue(`Input directory: ${this._input}`))
+
+    // Ensure output path is ok
+    this._output = path.resolve(output)
+    let stat
+    let createFile = false
+    let createDir = false
+    try {
+      stat = await fs.stat(this._output)
+    }
+    catch (err) {
+      if (err.code !== 'ENOENT') {
+        throw err
+      }
+      else {
+        if (this._output.endsWith(path.sep)) {
+          createDir = true
+        }
+        else {
+          createFile = true
+        }
+      }
+    }
+
+    // If output path is file, create new archive with the same path
+    if (createFile || stat.isFile()) {
+      logger.debug(chalk.blue(`Output path is file: ${this._output}`))
+      await this.archive(this._input, this._output)
+    }
+    // If output path is directory, create new archive in the directory with default file name
+    else if (createDir || stat.isDirectory()) {
+      logger.debug(chalk.blue(`Output path is directory: ${this._output}`))
+
+      await fse.ensureDir(this._output, { mode: 0o2775 })
+      await fs.access(this._output, fsConst.X_OK | fsConst.W_OK)
+
+      this._output = path.join(this._output, 'archive.zip')
+      await this.archive(this._input, this._output)
+    }
+    else {
+      throw new Error('Output path must be either a file path or a directory path.')
+    }
+
+    logger.info(chalk.green(`Create offline package successfully!`))
+  }
+
+  protected async archive (inputPath, outputPath) {
+    logger.info(chalk.green(`Start archiving input path ...`))
+
+    const output = createWriteStream(outputPath)
+    const archive = archiver('zip', {
+      zlib: { level: 9 } // Sets the compression level.
+    })
+
+    output.on('close', function() {
+      logger.info(chalk.green(`Archive done, total bytes: ${archive.pointer()}B`))
+    })
+
+    archive.on('warning', function(err) {
+      logger.warn(`Archiver warning: ${err.message}`)
+    })
+
+    archive.on('error', function(err) {
+      throw err
+    })
+
+    archive.pipe(output)
+
+    const map = {}
+    await Promise.all(this._map.map(async (pair): Promise<void> => {
+      const files = await glob(path.join(inputPath, pair.from, '**/*.*(html|js|css|png|jpg)'))
+      files.forEach(filePath => {
+        let mappedPath = filePath.replace(inputPath + path.sep, '')
+        mappedPath = path.join(pair.to, mappedPath)
+
+        if (mappedPath.match(/index\.html$/)) {
+          // /path/home/ => /path/home/index.html
+          const fromPath1 = mappedPath.replace(/index\.html$/, '')
+          map[fromPath1] = mappedPath
+          // /path/home => /path/home/index.html
+          const fromPath2 = mappedPath.replace(/\/index\.html$/, '')
+          map[fromPath2] = mappedPath
+        }
+        if (mappedPath.match(/\.html$/)) {
+          // /path/home/ => /path/home.html
+          const fromPath1 = mappedPath.replace(/\.html$/, '')
+          map[fromPath1] = mappedPath
+          // /path/home => /path/home.html
+          const fromPath2 = mappedPath.replace(/\/\.html$/, '')
+          map[fromPath2] = mappedPath
+        }
+
+        map[mappedPath] = mappedPath
+      })
+
+      archive.directory(path.join(inputPath, pair.from), path.join(this._pid, pair.to))
+    }))
+
+    logger.debug(chalk.blue(`Create map config: ${JSON.stringify(map, null, '  ')}`))
+
+    archive.append(JSON.stringify({ map }, null, '  '), { name: path.join(this._pid, 'config.json') })
+    archive.finalize()
+  }
+}
+
+export const packager = new Packager()
+
+export default packager
